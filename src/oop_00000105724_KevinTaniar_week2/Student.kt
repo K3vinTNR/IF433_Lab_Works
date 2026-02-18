edd366028cbc13e4398c4152aac3fb7af581cfb0
unknown <kevint101106@gmail.com>
2026-02-18 07:54:38 +0700

    week02: challenge refactor primary constructor using default args

--- src/oop_00000105724_KevinTaniar_week2/Student.kt
+++ src/oop_00000105724_KevinTaniar_week2/Student.kt
@@ -3,7 +3,8 @@ package oop_00000105724_KevinTaniar_week2
 class student (
     val name: String,
     val nim: String,
-    var major: String
+    var major: String,
+    var gpa: Double = 0.0
 ) {
     init {
         if (nim.length != 5) {
