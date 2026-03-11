321d5922add86f17f2f488cb1ad4a5bd8c5fb716
unknown <kevint101106@gmail.com>
2026-03-11 10:17:33 +0700

    week06: (task) instantiate smart devices in main

--- src/oop_00000105724_kevintaniar_week6/lab/Main.kt
+++ src/oop_00000105724_kevintaniar_week6/lab/Main.kt
@@ -1,7 +1,5 @@
 package oop_00000105724_kevintaniar_week6.lab
 
-// Fungsi ini adalah contoh Decoupling & Polymorphism
-// Ia tidak peduli apakah yang datang Gopay atau CreditCard, selama mematuhi kontrak PaymentMethod
 fun processCheckout(method: PaymentMethod, amount: Double) {
     println("-> Memulai checkout...")
     method.pay(amount)
@@ -15,10 +13,16 @@ fun main() {
     val myPhone = Smartphone()
     myPhone.turnOn()
 
-    // CHECKPOINT 12: Testing Checkout System
     println("\n=== TESTING CHECKOUT ===")
 
-    // Memasukkan objek yang berbeda ke fungsi yang sama
     processCheckout(Gopay(), 50000.0)
     processCheckout(CreditCard(), 150000.0)
+    val hub = SmartHomeHub()
+    val lamp = SmartLamp("L01", "Ruang Tamu")
+    val speaker = SmartSpeaker("S01", "Google Nest Dapur")
+    val cctv = SmartCCTV("C01", "Ezviz Garasi")
+
+    hub.addDevice(lamp)
+    hub.addDevice(speaker)
+    hub.addDevice(cctv)
 }
