59df737c24f82c99d03548ba13e7ffe02a6c66c6
unknown <kevint101106@gmail.com>
2026-04-14 17:53:41 +0700

    week07: test Singleton and Companion Object in main

--- src/oop_00000105724_kevintaniar_week7/lab/Main.kt
+++ src/oop_00000105724_kevintaniar_week7/lab/Main.kt
@@ -1,6 +1,11 @@
 package oop_00000105724_kevintaniar_week7.lab
 
 fun main() {
-    // Ini akan menyebabkan Compiler Error
-    // val client = NetworkClient("https://api.umn.ac.id")
+    println("=== TEST SINGLETON ===")
+    println("Status: ${DatabaseManager.connectionStatus}")
+    DatabaseManager.connect()
+
+    println("\n=== TEST COMPANION OBJECT ===")
+    val client = NetworkClient.createClient()
+    client.connect()
 }
