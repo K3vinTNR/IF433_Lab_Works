9dce6aac28b4e0d4109f84352dea3562e3ceb24a
unknown <kevint101106@gmail.com>
2026-04-14 18:01:18 +0700

    week07: (task) create GameManager singleton

---
+++ src/oop_00000105724_kevintaniar_week7/lab/GameManager.kt
@@ -0,0 +1,14 @@
+package oop_00000105724_kevintaniar_week7.lab
+
+object GameManager {
+    var isGameRunning: Boolean = false
+
+    fun startGame() {
+        if (isGameRunning) {
+            println("Game sudah berjalan! Mencegah instansiasi ganda.")
+        } else {
+            isGameRunning = true
+            println("Memulai Game Engine...")
+        }
+    }
+}
