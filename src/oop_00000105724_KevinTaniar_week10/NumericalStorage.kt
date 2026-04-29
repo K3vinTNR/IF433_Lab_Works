3967566a004c64123aae1cbdd0e2bf180605ce8b
unknown <kevint101106@gmail.com>
2026-04-29 10:58:08 +0700

    week10: test math operations and comparable constraints

--- src/oop_00000105724_KevinTaniar_week10/NumericalStorage.kt
+++ src/oop_00000105724_KevinTaniar_week10/NumericalStorage.kt
@@ -6,7 +6,8 @@ class MathBox<T: Number>(val value1: T, val value2: T) {
         return value1.toDouble() + value2.toDouble()
     }
     // Menggunakan where clause untuk batasan Comparable
-    fun <T> getMax(a: T, b: T): T where T: Comparable<T> {
-        return if (a > b) a else b
-    }
+
+}
+fun <T> getMax(a: T, b: T): T where T: Comparable<T> {
+    return if (a > b) a else b
 }
