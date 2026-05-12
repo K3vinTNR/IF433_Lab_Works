524d0a04df9e30ad75609babbcaf93e168bdb9fe
unknown <kevint101106@gmail.com>
2026-05-12 19:41:11 +0700

    week11: implement run function for object computation

--- src/oop_00000105724_KevinTaniar_week11/ScopeLetRun.kt
+++ src/oop_00000105724_KevinTaniar_week11/ScopeLetRun.kt
@@ -9,4 +9,11 @@ fun main() {
         it.length // Mengembalikan nilai expression terakhir
     }
     println("Panjang nama: $length")
+
+    println("\n=== TEST RUN FUNCTION ===")
+    val result = "Kotlin".run {
+        println("Memproses kata: ${this}") // 'this' merujuk pada "Kotlin"
+        this.length * 2 // Kalkulasi
+    }
+    println("Hasil kalkulasi run: $result")
 }
