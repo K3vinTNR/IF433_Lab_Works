49a5bc5d9e72a13c2a10f7847bf8a5928b6f3632
unknown <kevint101106@gmail.com>
2026-05-12 19:54:12 +0700

    week11: (task) format dashboard output using with

--- src/oop_00000105724_KevinTaniar_week11/SmartHomeDashboard.kt
+++ src/oop_00000105724_KevinTaniar_week11/SmartHomeDashboard.kt
@@ -25,4 +25,7 @@ fun main() {
     searchResult?.let {
         println(it.diagnose())
     }
+    with(homeDevices) {
+        println("Summary Dashboard - Total Perangkat: ${this.size}")
+    }
 }
