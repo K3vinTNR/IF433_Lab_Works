b45e280303da989979d33865cb8f7708a2b1247c
unknown <kevint101106@gmail.com>
2026-06-06 08:05:10 +0700

    week13: (task) implement serialization logic for CryptoTrade

--- src/oop_00000105724_KevinTaniar_week13/CryptoTradeModels.kt
+++ src/oop_00000105724_KevinTaniar_week13/CryptoTradeModels.kt
@@ -1,3 +1,12 @@
 package oop_00000105724_KevinTaniar_week13
 
 data class CryptoTrade(val id: String, val asset: String, val type: String, val pnl: Double)
+
+fun List<CryptoTrade>.toCsvFormat(): String {
+    val sb = StringBuilder()
+    sb.append("ID,Asset,Type,PnL\n")
+    for (trade in this) {
+        sb.append("${trade.id},${trade.asset},${trade.type},${trade.pnl}\n")
+    }
+    return sb.toString()
+}
