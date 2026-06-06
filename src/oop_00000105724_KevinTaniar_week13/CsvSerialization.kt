be7b7c22cfbecbae30fa786b36d0e144a2922c01
unknown <kevint101106@gmail.com>
2026-06-06 07:58:51 +0700

    week13: serialize student list to csv string

--- src/oop_00000105724_KevinTaniar_week13/CsvSerialization.kt
+++ src/oop_00000105724_KevinTaniar_week13/CsvSerialization.kt
@@ -3,3 +3,12 @@ package oop_00000105724_KevinTaniar_week13
 import java.io.File
 
 data class Student(val nim: String, val name: String, val gpa: Double)
+
+fun List<Student>.toCsvString(): String {
+    val sb = StringBuilder()
+    sb.append("NIM,Nama,IPK\n") // Header CSV
+    for (student in this) {
+        sb.append("${student.nim},${student.name},${student.gpa}\n")
+    }
+    return sb.toString()
+}
