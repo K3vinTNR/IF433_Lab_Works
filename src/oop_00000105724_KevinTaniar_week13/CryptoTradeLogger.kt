9fd3fbea40d6c2ff1502b20aa76d1c153177694b
unknown <kevint101106@gmail.com>
2026-06-06 08:15:46 +0700

    week13: (task) initialize trades list in main

---
+++ src/oop_00000105724_KevinTaniar_week13/CryptoTradeLogger.kt
@@ -0,0 +1,12 @@
+package oop_00000105724_KevinTaniar_week13
+
+import java.io.File
+
+fun main() {
+    println("=== INITIALIZING CRYPTO TRADES ===")
+    val initialTrades = mutableListOf(
+        CryptoTrade("TX101", "BTC", "BUY", 150.50),
+        CryptoTrade("TX102", "ETH", "SELL", -45.20),
+        CryptoTrade("TX103", "SOL", "BUY", 320.00)
+    )
+}
